0831b7105d028e7e86eac1682f5a4e19ff2dd587
Hugo Mota <hugo.txt@gmail.com>
2017-10-28 11:26:27 -0200

    Remove obsolete code from the store
    The actions structure works great, but if it's embeded in the store
    it ends up making it more hard to use, and limits the usage of
    multiple stores in an application.

--- src/true-store.js
+++ src/true-store.js
@@ -4,17 +4,8 @@ const Immutable = require('immutable');
 class TrueStore {
 
     constructor(initialState) {
-        initialState = initialState || {};
-        this.currentStateMap = Immutable.fromJS(initialState);
-        this.nextStateObject = initialState;
-        this.registeredActionNames = [];
+        this.currentStateMap = Immutable.fromJS(initialState || {});
         this.dataListeners = {};
-        this.actionListeners = {};
-        this.debug = false;
-    }
-
-    state() {
-        return this.currentStateMap.toJS();
     }
 
     get(key) {
@@ -32,36 +23,6 @@ class TrueStore {
         this.executeDataListeners(oldStateMap, this.currentStateMap);
     }
 
-    action(name, func) {
-        var self = this;
-        this.validateActionArguments(name, func);
-        this.registeredActionNames.push(name);
-        return function() {
-            var args = self.argumentsToArray(arguments);
-            if (self.debug)
-                console.log(name, Immutable.fromJS(args).toJS());
-            args.unshift(self.nextStateObject);
-            func.apply(null, args);
-            var oldStateMap = self.currentStateMap;
-            self.currentStateMap = Immutable.fromJS(self.nextStateObject);
-            self.executeDataListeners(oldStateMap, self.currentStateMap);
-            self.executeActionListeners(name);
-        };
-    }
-
-    argumentsToArray(args) {
-        return args.length === 1 ? [args[0]] : Array.apply(null, args);
-    }
-
-    validateActionArguments(name, func) {
-        if (!name || typeof(name) != 'string')
-            throw Error('TrueStore.action: invalid argument "name".');
-        if (!func || typeof(func) != 'function')
-            throw Error('TrueStore.action: invalid argument "func".');
-        if (this.registeredActionNames.indexOf(name) !== -1)
-            throw Error('TrueStore.action: duplicate action name "' + name + '"');
-    }
-
     listenData(key, callback) {
         this.dataListeners[key] = this.dataListeners[key] || [];
         this.dataListeners[key].push(callback);
@@ -75,19 +36,6 @@ class TrueStore {
         this.dataListeners[key].splice(index, 1);
     }
 
-    listenAction(name, callback) {
-        this.actionListeners[name] = this.actionListeners[name] || [];
-        this.actionListeners[name].push(callback);
-    }
-
-    unlistenAction(name, callback) {
-        this.actionListeners[name] = this.actionListeners[name] || [];
-        var index = this.actionListeners[name].indexOf(callback);
-        if (index == -1)
-            throw 'TrueStore.unlistenAction: action "' + name + '" is not registered.';
-        this.actionListeners[name].splice(index, 1);
-    }
-
     executeDataListeners(oldMap, newMap) {
         for (var key in this.dataListeners)
             this.dataListeners[key].map((callback) => {
@@ -95,14 +43,6 @@ class TrueStore {
                 var oldValue = oldMap.getIn(pathArray);
                 var newValue = newMap.getIn(pathArray);
                 if (!Immutable.is(oldValue, newValue))
-                    callback();
-            });
-    }
-
-    executeActionListeners(actionName) {
-        for (var name in this.actionListeners)
-            this.actionListeners[name].map((callback) => {
-                if (name == actionName)
                     callback();
             });
     }
--- tests/true-store.test.js
+++ tests/true-store.test.js
@@ -13,40 +13,6 @@ describe('constructor', () => {
     });
 });
 
-describe('state', () => {
-
-    it('returns a copy of the state', () => {
-        var store = new TrueStore({foo: 42});
-        var state = store.state();
-        expect(state.foo).toBe(42);
-    });
-
-    it('returns an empty object if store was created without arguments', () => {
-        var store = new TrueStore();
-        expect(store.state()).toEqual({});
-    });
-
-    it('does not compute changes in the state copy', () => {
-        var store = new TrueStore({foo: 42});
-        var state1 = store.state();
-        state1.foo = 45;
-        var state2 = store.state();
-        expect(state2.foo).toBe(42);
-    });
-
-    it('does not allow nested state values to be written outside actions', () => {
-        var store = new TrueStore({
-            users: [
-                {id: 1, name: 'John'}
-            ]
-        });
-        var state1 = store.state();
-        state1.users[0].name = 'Jane';
-        var state2 = store.state();
-        expect(state2.users[0].name).toBe('John');
-    });
-});
-
 describe('get', () => {
 
     it('returns a portion of the state', () => {
@@ -143,107 +109,23 @@ describe('set', () => {
     });
 });
 
-describe('action', () => {
-
-    it('creates a new function', () => {
-        var store = new TrueStore();
-        var actionFunction = function() {};
-        var action = store.action('actionName', actionFunction);
-        expect(action).toBeInstanceOf(Function);
-        expect(action).not.toBe(actionFunction);
-    });
-
-    it('cannot be created without a name', () => {
-        var store = new TrueStore();
-        expect(() => {
-            var action = store.action('', function() {});
-        }).toThrow();
-    });
-
-    it('cannot be created without a function', () => {
-        var store = new TrueStore();
-        expect(() => {
-            var action = store.action('actionName');
-        }).toThrow();
-    });
-
-    it('can change the store state', () => {
-        var store = new TrueStore({foo: 42});
-        var action = store.action('fooAction', function(state, value) {
-            state.foo = value;
-        });
-        action('bar');
-        expect(store.state()).toEqual({foo: 'bar'});
-    });
-
-    it('cannot register two actions with the same name', () => {
-        var store = new TrueStore();
-        store.action('fooAction', function() {});
-        expect(() => {
-            store.action('fooAction', function() {});
-        }).toThrow();
-    });
-
-    it('works with arrow functions', () => {
-        var store = new TrueStore({foo: 42});
-        var action = store.action('fooAction', (state, value) => {
-            state.foo = value;
-        });
-        action('bar');
-        expect(store.state()).toEqual({foo: 'bar'});
-    });
-
-    it('persists data changes through multiple actions', () => {
-        var store = new TrueStore({foo: null, bar: null});
-        var fooAction = store.action('fooAction', (state) => {
-            state.foo = 'foo';
-        });
-        var barAction = store.action('barAction', (state) => {
-            state.bar = 'bar';
-        });
-        fooAction();
-        barAction();
-        expect(store.state()).toEqual({foo: 'foo', bar: 'bar'});
-    });
-
-    it('persists data changes through chained/nested actions', () => {
-        var store = new TrueStore({foo: null, bar: null});
-        store.debug = true;
-        var fooAction = store.action('fooAction', (state, name) => {
-            state.foo = name;
-            barAction('bar');
-        });
-        var barAction = store.action('barAction', (state, name) => {
-            state.bar = name;
-        });
-        fooAction('foo');
-        expect(store.state()).toEqual({foo: 'foo', bar: 'bar'});
-    });
-});
-
 describe('listenData', () => {
 
     it('executes a callback when observed data changes', () => {
         var store = new TrueStore({foo: 42});
-        var action = store.action('fooAction', function(state) {
-            state.foo = 'bar';
-        });
         var callback = jest.fn();
         store.listenData('foo', callback);
-        action();
+        store.set('foo', 'bar');
         expect(callback).toHaveBeenCalled();
     });
 
     it('can execute two callbacks when observed data changes', () => {
         var store = new TrueStore({foo: 42});
-        var action = store.action('fooAction', function(state) {
-            state.foo = 'bar';
-        });
         var callback1 = jest.fn();
         var callback2 = jest.fn();
         store.listenData('foo', callback1);
         store.listenData('foo', callback2);
-        action();
+        store.set('foo', 'bar');
         expect(callback1).toHaveBeenCalled();
         expect(callback2).toHaveBeenCalled();
     });
@@ -254,60 +136,26 @@ describe('listenData', () => {
                 users: [{id: 1, name: 'John'}]
             }
         });
-        var action = store.action('fooAction', function(state) {
-            state.database.users[0].name = 'Jane';
-        });
         var callback = jest.fn();
         store.listenData('database.users', callback);
-        action();
+        store.set('database.users.0.name', 'Jane');
         expect(callback).toHaveBeenCalled();
     });
 
-    it('does not execute the callback if nothing changes', () => {
-        var store = new TrueStore();
-        var action = store.action('fooAction', function() {});
-        var callback = jest.fn();
-        store.listenData('foo', callback);
-        action();
-        expect(callback).not.toHaveBeenCalled();
-    });
-
     it('does not execute the callback if unobserved data changes', () => {
-        var store = new TrueStore({foo: 42});
-        var action = store.action('fooAction', function(state) {
-            state.foo = 'bar';
-        });
+        var store = new TrueStore({foo: 42, bar: 42});
         var callback = jest.fn();
         store.listenData('bar', callback);
-        action();
+        store.set('foo', 43);
         expect(callback).not.toHaveBeenCalled();
     });
 
     it('does not execute the callback if data changes to the same value', () => {
         var store = new TrueStore({foo: 42});
-        var action = store.action('fooAction', function(state) {
-            state.foo = 42;
-        });
         var callback = jest.fn();
         store.listenData('foo', callback);
-        action();
+        store.set('foo', 42);
         expect(callback).not.toHaveBeenCalled();
-    });
-
-    it('does not execute callback twice in chained actions', () => {
-        var store = new TrueStore({foo: null});
-        var fooAction = store.action('fooAction', function(state) {
-            state.foo = 'foo';
-            barAction();
-        });
-        var barAction = store.action('barAction', function(state) {
-            state.foo = 'bar';
-        });
-        var callback = jest.fn();
-        store.listenData('foo', callback);
-        fooAction();
-        expect(callback).toHaveBeenCalled();
-        expect(callback.mock.calls.length).toEqual(1);
     });
 });
 
@@ -315,13 +163,10 @@ describe('unlistenData', () => {
 
     it('prevents previously registered callback from executing', () => {
         var store = new TrueStore({foo: 42});
-        var action = store.action('fooAction', function(state) {
-            state.foo = 'bar';
-        });
         var callback = jest.fn();
         store.listenData('foo', callback);
         store.unlistenData('foo', callback);
-        action();
+        store.set('foo', 'bar');
         expect(callback).not.toHaveBeenCalled();
     });
 
@@ -340,115 +185,5 @@ describe('unlistenData', () => {
         expect(() => {
             store.unlistenData('bar', callback);
         }).toThrow();
-    });
-});
-
-describe('listenAction', () => {
-
-    it('executes a callback when observed action gets executed', () => {
-        var store = new TrueStore();
-        var action = store.action('fooAction', function() {});
-        var callback = jest.fn();
-        store.listenAction('fooAction', callback);
-        action();
-        expect(callback).toHaveBeenCalled();
-    });
-
-    it('can execute two callbacks when observed action gets executed', () => {
-        var store = new TrueStore();
-        var action = store.action('fooAction', function() {});
-        var callback1 = jest.fn();
-        var callback2 = jest.fn();
-        store.listenAction('fooAction', callback1);
-        store.listenAction('fooAction', callback2);
-        action();
-        expect(callback1).toHaveBeenCalled();
-        expect(callback2).toHaveBeenCalled();
-    });
-
-    it('does not execute the callback when unobserved action gets executed', () => {
-        var store = new TrueStore();
-        var fooAction = store.action('fooAction', function() {});
-        var barAction = store.action('barAction', function() {});
-        var callback = jest.fn();
-        store.listenAction('barAction', callback);
-        fooAction();
-        expect(callback).not.toHaveBeenCalled();
-    });
-
-    it('allow actions to be listened before declaration', () => {
-        var store = new TrueStore();
-        var callback = jest.fn();
-        expect(() => {
-            store.listenAction('fooAction', callback);
-        }).not.toThrow();
-    });
-});
-
-describe('unlistenAction', () => {
-
-    it('prevents previously registered callback from executing', () => {
-        var store = new TrueStore();
-        var action = store.action('fooAction', function() {});
-        var callback = jest.fn();
-        store.listenAction('fooAction', callback);
-        store.unlistenAction('fooAction', callback);
-        action();
-        expect(callback).not.toHaveBeenCalled();
-    });
-
-    it('throws an error if trying to unlisten an unregistered callback', () => {
-        var store = new TrueStore();
-        var action = store.action('fooAction', function() {});
-        var callback = jest.fn();
-        expect(() => {
-            store.unlistenAction('fooAction', callback);
-        }).toThrow();
-    });
-
-    it('throws an error if trying to unlisten a callback registered for other key', () => {
-        var store = new TrueStore();
-        var action = store.action('fooAction', function() {});
-        var callback = jest.fn();
-        store.listenAction('fooAction', callback);
-        expect(() => {
-            store.unlistenAction('barAction', callback);
-        }).toThrow();
-    });
-});
-
-describe('debug', () => {
-
-    beforeEach(() => {
-        console.originalLog = console.log;
-        console.log = jest.fn();
-    });
-
-    afterEach(() => {
-        console.log = console.originalLog;
-    });
-
-    it('logs the action names and arguments if turned on', () => {
-        var store = new TrueStore();
-        store.debug = true;
-        var action = store.action('fooAction', function(state, bar) {});
-        action('bar');
-        expect(console.log).toHaveBeenCalled();
-        expect(console.log.mock.calls.length).toBe(1);
-        expect(console.log.mock.calls[0][0]).toBe('fooAction');
-        expect(console.log.mock.calls[0][1]).toContain('bar');
-    });
-
-    it('does not log anything when turned off', () => {
-        var store = new TrueStore();
-        store.debug = false;
-        var action = store.action('fooAction', function(bar) {});
-        action('bar');
-        expect(console.log).not.toHaveBeenCalled();
-    });
-
-    it('starts turned off', () => {
-        var store = new TrueStore();
-        expect(store.debug).toBe(false);
     });
 });
